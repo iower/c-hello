e214fcff4aeb772e62f30332566a97ad26c61219
iower <iower@tutanota.com>
2026-04-20 11:51:10 +0500

    pointer 3

--- 26-pointer.c
+++ 26-pointer.c
@@ -15,4 +15,16 @@ void main() {
 	p_p_a = &p_a;
 
 	printf("%x \n", **p_p_a);
+
+	printf("--- \n");
+
+	unsigned char uch[10] = {0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC};
+	unsigned char *p_uch = &uch;
+	printf("%x \n", p_uch);
+	printf("%x \n", *p_uch);
+
+	for (unsigned char i = 0; i < 12; i++) {
+		unsigned char *p_uch2 = &uch[i];
+		printf("%u | %x | %x \n", i, p_uch2, *p_uch2);
+	}
 }
