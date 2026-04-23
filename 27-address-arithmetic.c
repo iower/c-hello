bf158304653127fccc59f69718e29cc79c724e70
iower <iower@tutanota.com>
2026-04-23 10:51:01 +0500

    address arithmetic 2

--- 27-address-arithmetic.c
+++ 27-address-arithmetic.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void main() {
 	unsigned int a[10] = {0x33333333, 0x44444444, 0x55555555, 0x66666666, 0x77777777, 0x88888888, 0x99999999, 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC};
 	unsigned int *p0_a, *p5_a;
@@ -5,6 +7,34 @@ void main() {
 	p5_a = &a[5];
 
 	for (unsigned int i = 0; i < 10; i++) {
-		printf("%i | %i | %p | %x \n", i, sizeof(&a[i]), &a[i], &a[i]);
+		printf("%i | %li | %p | %x \n", i, sizeof(&a[i]), &a[i], a[i]);
 	}
+
+	printf("\n");
+
+	printf("%p | %x \n", p0_a, *p0_a);
+	printf("%p | %x \n\n", p5_a, *p5_a);
+
+	p0_a++;
+	p5_a++;
+	printf("%p | %x \n", p0_a, *p0_a);
+	printf("%p | %x \n\n", p5_a, *p5_a);
+
+	p0_a--;
+	p5_a--;
+	printf("%p | %x \n", p0_a, *p0_a);
+	printf("%p | %x \n\n", p5_a, *p5_a);
+
+	p0_a += 3;
+	p5_a += 3;
+	printf("%p | %x \n", p0_a, *p0_a);
+	printf("%p | %x \n\n", p5_a, *p5_a);
+
+	p0_a -= 2;
+	p5_a -= 2;
+	printf("%p | %x \n", p0_a, *p0_a);
+	printf("%p | %x \n\n", p5_a, *p5_a);
+
+	unsigned int n = p5_a - p0_a;
+	printf("%i \n\n", n);
 }
