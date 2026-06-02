f225c85c9b4634fab9d4a16ae55a105de5824676
iower <iower@tutanota.com>
2026-06-02 10:28:39 +0500

    pointer arguments 3

--- 29-arguments-pointers.c
+++ 29-arguments-pointers.c
@@ -27,6 +27,16 @@ void print_uint32_arr(const unsigned int *p_uint, unsigned int len)
 	}
 }
 
+void my_sum_p(float a_f, float b_f, float *sum_f)
+{
+	*sum_f = a_f + b_f;
+}
+
+void my_div_p(float a_f, float b_f,float *div_f)
+{
+	*div_f = a_f / b_f;
+}
+
 void main()
 {
 	char c_str[] = "a";
@@ -64,4 +74,11 @@ void main()
 
 	print_uint32_arr(a, 10);
 	printf("-\n");
+
+	float res = .0;
+	my_sum_p(1.11, 2.22, &res);
+	printf("sum %f\n", res);
+
+	my_div_p(10., 3., &res);
+	printf("sum %f\n", res);
 }
