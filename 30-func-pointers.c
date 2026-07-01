168be33c4d0e0e267f758c01ba233507b930609f
iower <iower@tutanota.com>
2026-07-01 10:47:19 +0500

    func pointers 2

--- 30-func-pointers.c
+++ 30-func-pointers.c
@@ -5,6 +5,16 @@ void print_str(const char *c_str)
 	puts(c_str);
 }
 
+void my_mul_p(float a_f, float b_f, float *mul_f)
+{
+  *mul_f = a_f * b_f;
+}
+
+void my_sub_p(float a_f, float b_f, float *sub_f)
+{
+  *sub_f = a_f - b_f;
+}
+
 void main()
 {
 	unsigned int *print_str_addr;
@@ -14,4 +24,16 @@ void main()
 	void (*print_str_new)(const char *c_str);
 	print_str_new = (void *)print_str_addr;
 	print_str_new("Hi!");
+
+	print_str_new = print_str;
+	print_str_new("Hi2!");
+
+	void (*arith_operations[2])(float, float, float *) = {my_mul_p, my_sub_p};
+	float a = 3., b = 5., res = .0;
+
+	for(unsigned char i=0; i<2; i++)
+	{
+		arith_operations[i](a, b, &res);
+		printf("Op %d: res = %f\n", i, res);
+	}
 }
