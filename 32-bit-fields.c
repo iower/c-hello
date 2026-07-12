58fdc2edb95fff17c1f2c14ec14cb64d55b4bc1f
iower <iower@tutanota.com>
2026-07-12 10:52:52 +0500

    bit-fields

---
+++ 32-bit-fields.c
@@ -0,0 +1,15 @@
+#pragma pack(push, 1)
+typedef struct
+{
+	unsigned char diad0:2;
+	unsigned char tri0:3;
+	unsigned char bit0:1;
+	unsigned char bit1:1;
+	unsigned char a;
+	unsigned short b;
+} my_arg_t;
+#pragma pack(pop)
+
+void main()
+{
+}
