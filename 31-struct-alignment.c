ec616cf2fab9ce3de373834d4163a4825115d885
iower <iower@tutanota.com>
2026-07-12 10:26:14 +0500

    union

--- 31-struct-alignment.c
+++ 31-struct-alignment.c
@@ -2,20 +2,27 @@
 
 float yf, zf;
 
-//#pragma pack(push, 1)
-#pragma scalar_storage_order big-endian
+#pragma pack(push, 1)
+//#pragma scalar_storage_order big-endian
 typedef struct
 {
 	unsigned char x;
 	unsigned short y;
 	unsigned int z;
 } my_arg_t;
-#pragma scalar_storage_order little-endian
-//#pragma pack(pop)
+#pragma pack(pop)
+//#pragma scalar_storage_order little-endian
+
+typedef union
+{
+	unsigned char x;
+	unsigned short y;
+	unsigned int z;
+} my_arg_u;
 
 void main()
 {
-	printf("sizeof data is %zu\n", sizeof(my_arg_t));
+	printf("sizeof struct is %zu\n", sizeof(my_arg_t));
 
 	my_arg_t my_arg;
 	my_arg.x = 0x11;
@@ -29,4 +36,24 @@ void main()
 	printf("addr x = 0x%X\n", &my_arg.x);
 	// printf("addr y = 0x%X\n", &my_arg.y);
 	printf("addr z = 0x%X\n", &my_arg.z);
+
+	//
+
+	printf("sizeof union is %zu\n", sizeof(my_arg_u));
+
+	my_arg_u my_arg_2;
+	my_arg_2.x = 0x11;
+	// my_arg.y = 0x2233;
+	my_arg_2.z = 0x44556677;
+
+	printf("x = 0x%X\n", my_arg_2.x);
+	// printf("y = 0x%X\n", my_arg_2.y);
+	printf("z = 0x%X\n", my_arg_2.z);
+
+	printf("addr x = 0x%X\n", &my_arg_2.x);
+	// printf("addr y = 0x%X\n", &my_arg_2.y);
+	printf("addr z = 0x%X\n", &my_arg_2.z);
+
+	my_arg_2.x = 0x99;
+	printf("z = 0x%X\n", my_arg_2.z);
 }
